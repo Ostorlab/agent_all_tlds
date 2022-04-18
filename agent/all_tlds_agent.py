38b7aa181b2fef1d07c73a49ca0091bed6239dfd
chevalier3as <chevalier3as@gmail.com>
2022-04-18 14:50:50 +0000

    Full implementation of the all TLDs agent.

---
+++ agent/all_tlds_agent.py
@@ -0,0 +1,75 @@
+"""All TLDs agent implementation"""
+import logging
+from rich import logging as rich_logging
+
+import tld
+from typing import Iterator
+from ostorlab.agent import agent
+from ostorlab.agent import message as m
+from ostorlab.agent.mixins import agent_persist_mixin
+from ostorlab.agent import definitions as agent_definitions
+from ostorlab.runtimes import definitions as runtime_definitions
+
+logging.basicConfig(
+    format='%(message)s',
+    datefmt='[%X]',
+    level='INFO',
+    force=True,
+    handlers=[rich_logging.RichHandler(rich_tracebacks=True)]
+)
+logger = logging.getLogger(__name__)
+
+STORAGE_NAME = 'agent_all_tlds_storage'
+
+
+class AllTldsAgent(agent.Agent, agent_persist_mixin.AgentPersistMixin):
+    """Hello world agent."""
+
+    def __init__(self,
+                 agent_definition: agent_definitions.AgentDefinition,
+                 agent_settings: runtime_definitions.AgentSettings) -> None:
+
+        agent.Agent.__init__(self, agent_definition, agent_settings)
+        agent_persist_mixin.AgentPersistMixin.__init__(self, agent_settings)
+
+    def process(self, message: m.Message) -> None:
+        """Generates all possible TLD iteration of a source domain.
+
+        Args:
+            message: Domain to process.
+
+        Returns:
+            None
+        """
+        domain_tld = tld.get_tld(message.data.get('name'), fix_protocol=True, as_object=True, fail_silently=True)
+        if self.set_add(STORAGE_NAME, domain_tld.domain) is False:
+            return
+
+        tlds = tld.get_tld_names()
+        for t in _generate_tlds(tlds):
+            self.emit('v3.asset.domain_name', {'name': f'{domain_tld.domain}{t}'})
+
+
+def _generate_tlds(tlds) -> Iterator[str]:
+    """Generates all TLDs iterations."""
+    for root in tlds.values():
+        node = root.root
+        yield from _get_child_tlds('', node)
+
+
+def _get_child_tlds(tld, node) -> Iterator[str]:
+    """Recursive implementation to compose TLDs with their children."""
+    for c_tld, c_node in node.children.items():
+        if c_tld == '*':
+            yield tld
+        else:
+            new_tld = f'.{c_tld}{tld}'
+            if c_node.leaf is True:
+                yield new_tld
+            else:
+                yield from _get_child_tlds(new_tld, c_node)
+
+
+if __name__ == '__main__':
+    logger.info('starting agent ...')
+    AllTldsAgent.main()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,26 @@
-"""
-    Dummy conftest.py for template_agent.
+"""Pytest fixtures for the Subfinder agent"""
+import random
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
-"""
+import pytest
+import pathlib
 
-# import pytest
+from ostorlab.agent import definitions as agent_definitions
+from ostorlab.runtimes import definitions as runtime_definitions
+
+from agent import all_tlds_agent
+
+
+@pytest.fixture(scope='function', name='all_tlds_agent')
+def all_tlds_agent_object():
+    with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
+        definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
+        settings = runtime_definitions.AgentSettings(
+            key='agent/ostorlab/all_tlds',
+            bus_url='NA',
+            bus_exchange_topic='NA',
+            args=[],
+            healthcheck_port=random.randint(5000, 6000),
+            redis_url='redis://guest:guest@localhost:6379')
+
+        agent = all_tlds_agent.AllTldsAgent(definition, settings)
+        return agent
